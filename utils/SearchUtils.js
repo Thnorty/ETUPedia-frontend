2b20364caf29446a54b815f939615c554c4c1cd1
Thnorty <ototatik@gmail.com>
2025-03-15 06:57:02 +0300

    fix: correct regex for Turkish character replacement in customFilter

--- utils/SearchUtils.js
+++ utils/SearchUtils.js
@@ -1,4 +1,4 @@
 export const customFilter = (data, searchTerm) => {
-  return data.replace(/[ıİ]/g, 'i').replace('i̇', 'i').toLowerCase()
-    .includes(searchTerm.replace(/[ıİ]/g, 'i').replace('i̇', 'i').toLowerCase());
+  return data.replace(/[ıİ]/g, 'i').replace(/i̇/g, 'i').toLowerCase()
+    .includes(searchTerm.replace(/[ıİ]/g, 'i').replace(/i̇/g, 'i').toLowerCase());
 }
